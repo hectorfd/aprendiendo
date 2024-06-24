a11e0272c6b7522bc3822bdb69793999b7e74352
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-06-24 17:22:54 -0500

    listas de cursos

---
+++ lib/vistas/paginas/maqueta/home_screen.dart
@@ -0,0 +1,232 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget{
+  
+  List catNames = [
+    'Categoría',
+    'Clases',
+    'Cursos Libres',
+    'Tienda de Libros',
+    'Cursos en vivo',
+    'Panel de Control'
+  ];
+
+  List<Color> setColors=[
+    Color(0xFFFFCF2F),
+    Color(0xFF6FE08D),
+    Color(0xFF61BDFD),
+    Color(0xFFFC7C7F),
+    Color(0xFFCB84FB),
+    Color(0xFF78E667),
+    
+  ];
+
+  List<Icon>catIcon = [
+    Icon(Icons.category, color: Colors.white, size: 30),
+    Icon(Icons.video_library, color: Colors.white, size: 30),
+    Icon(Icons.assignment, color: Colors.white, size: 30),
+    Icon(Icons.store, color: Colors.white, size: 30),
+    Icon(Icons.play_circle_fill, color: Colors.white, size: 30),
+    Icon(Icons.emoji_events, color: Colors.white, size: 30),
+
+  ];
+
+  List<String> imgList =[
+    'Flutter',
+    'React Native',
+    'Python',
+    'C#',
+  ];
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding:const EdgeInsets.only(
+              top: 15,
+              left: 15, 
+              right: 15,
+              bottom: 10,
+            ),
+            decoration:const BoxDecoration(
+              color: Color(0xFF6655F3),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+                
+                ),
+
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                      ),
+                      Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                      ),
+                  ],
+                ),
+                const SizedBox(height: 20,),
+                const Padding(
+                  padding:EdgeInsets.only(
+                    left:3,
+                    bottom: 15
+                    ),
+                    child: Text(
+                      'Hola, programador',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                      ), 
+                  ),
+                  Container(
+                    margin:const EdgeInsets.only(top: 5, bottom: 20),
+                    width: MediaQuery.of(context).size.width,
+                    height: 55,
+                    alignment: Alignment.center,
+                    decoration:const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                    ),
+                    child: TextFormField(
+                      decoration:const InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search,size: 25,),
+                        hintText: 'Busqueda aqui...',
+                        hintStyle: TextStyle(
+                          color: Color.fromARGB(255, 98, 96, 105),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          Padding(
+            padding:const EdgeInsets.only(top:20, left: 15,right: 15),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: catNames.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.1,
+            ),
+            itemBuilder: (context, index){
+              return Column(
+                  children: [
+                    Container(
+                        height: 60,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          color: setColors[index],
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: catIcon[index],
+                        ),
+                    ),
+                    const SizedBox(height: 10,),
+                    Text(
+                      catNames[index],
+                      style:const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0XFF39434C),
+                      ),
+                    ),
+                  ],
+              );
+            },
+            ),
+           const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'cursos',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Mostrar todo',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0XFF6655F3),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10,),
+            GridView.builder(
+              itemCount: imgList.length,
+              shrinkWrap: true,
+              physics:const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: (MediaQuery.of(context).size.height - 50 -25)/4*240,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+              ), itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding:const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color:const Color(0xFFF5F3FF),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(padding:const EdgeInsets.all(10),
+                        child: Image.asset(
+                          'images/${imgList[index]}.png',
+                          width: 50,
+                          height: 50,
+                          
+                          ),
+                        ),
+                        const SizedBox(height: 10,),
+                        Text(imgList[index]),
+
+                      ],
+                    ),
+                  ),
+
+
+                );
+                },
+
+            ),
+
+          ],
+          ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
--- lib/vistas/paginas/maqueta/welcome_screen.dart
+++ lib/vistas/paginas/maqueta/welcome_screen.dart
@@ -1,4 +1,6 @@
+import 'package:aprendiendo/vistas/paginas/maqueta/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WelcomeScreen extends StatelessWidget{
   const WelcomeScreen({super.key});
@@ -13,11 +15,101 @@ class WelcomeScreen extends StatelessWidget{
           children: [
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
+              height: MediaQuery.of(context).size.height/1.6,
+              decoration:const BoxDecoration(
+                color:  Colors.white,
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height/1.6,
+              decoration:const BoxDecoration(
+                color:  Color(0xFF6655F3),
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(70)),
+              ),
+              child: Center(
+                child:Image.asset('images/books.png', scale: 0.8,height: 300,),
+                
+                ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height/2.666,
+                decoration:const BoxDecoration(
+                  color: Color(0xFF6655F3),
+                ),
+              ),  
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height/2.666,
+                padding:const EdgeInsets.only(top:40, bottom: 30),
+                decoration:const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius. only(topLeft: Radius.circular(70)),
+                ),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Siempre se aprende',
+                      style:TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF6655F3),
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                      ),
+                      const SizedBox(height: 15,),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40),
+                        child: Text(
+                          'Aprende a programar o muere en el intento',
+                          style: TextStyle(
+                            fontSize: 17,
+                            color: Color(0xFF171520),
+                          ),
+                          ),
+                        ),
+                      const SizedBox(height: 60),
+                      Material(
+                        color:const  Color(0xFF6655F3),
+                        borderRadius:const BorderRadius.all(Radius.circular(20)),
+                        //borderRadius: BorderRadius. only(topLeft: Radius.circular(20)),
+                        child: InkWell(
+                          onTap: (){
+                            Navigator.push(context, MaterialPageRoute(
+                              builder: (context)=> HomeScreen()
+                              ));
+                          },
+                          child: Container(
+                            padding:const EdgeInsets.symmetric(
+                              vertical: 15,
+                              horizontal: 80,
+                              ),
+                            child:const Text(
+                              'Iniciar',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                              ),
+                          ),
+                        ),
+                      ),
+                    
+                  ],
+                ),
+              ),  
             ),
           ],
         ),
-
       ),
     );
   }
