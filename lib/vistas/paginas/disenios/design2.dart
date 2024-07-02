27c8cd7477cec4a50a31bf304daed07d01bed9bd
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-07-02 13:15:15 -0500

    Manchester City

--- lib/vistas/paginas/disenios/design2.dart
+++ lib/vistas/paginas/disenios/design2.dart
@@ -5,7 +5,252 @@ class Design2 extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Imagen de fondo
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            //?: redondear imagen con ClipRREct
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              child: Image.network(
+                'https://cdn1.unitedinfocus.com/uploads/14/2023/10/GettyImages-1748851913-1140x810.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            
+          ),
+          const Positioned.fill(
+            top: 480,
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Text(
+                'Cillum commodo excepteur non velit do cillum ullamco. Magna Lorem ex et reprehenderit adipisicing veniam culpa fugiat nulla. Commodo occaecat do proident ad. Proident culpa occaecat consectetur ullamco eu eu eu culpa occaecat consectetur ullamco.',
+                style: const TextStyle(
+                  color: Color(0xFF828287),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            ),
+          
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              iconTheme:const IconThemeData(
+                color: Colors.white, 
+                size: 30, 
+              ),
+              actions: [
+                
+                Container(
+                  //bordes redondeados
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.white.withOpacity(0.9),
+                    ),
+                  margin: const EdgeInsets.only(right: 20),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.heart_broken_rounded,
+                      color: Colors.red, // Color del ícono
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // titulo
+          Positioned(
+            top: 340,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding:const  EdgeInsets.all(30),
+              child:const Text(
+                'England Defender to leave Manchester City',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                  ),
+                ),
+            ),
+          ),
+          //fecha
+          Positioned(
+            top: 435,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding:const  EdgeInsets.all(30),
+              child:const Text(
+                'Thursday 01 July 2024 | England',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14.5,
+                  fontWeight: FontWeight.w500,
+                  ),
+                  ),
+                  ),
+                  ),
+                  //párrafo con efecto transparente en bottom
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 300,
+                      //color: Colors.white,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Colors.white.withOpacity(0.9),
+                            Colors.white.withOpacity(0.7),
+                            Colors.white.withOpacity(0.2),
+                          
+                            ],
+                      ),
+                    ),
+                  ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 300,
+                      //color: Colors.white,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          colors: [
+                            Colors.white.withOpacity(0.9),
+                            Colors.white.withOpacity(0.7),
+                            Colors.white.withOpacity(0.2),
+                          
+                            ],
+                      ),
+                    ),
+                  ),
+                  ),
+                  //crear boon read more
+                  Positioned(
+                    bottom: 150,
+                    left: 330,
+                    right: 0,
+                    child: Container(
+                    padding:const  EdgeInsets.all(30),
+                            child:const Text(
+                            'Ver mas',
+                            style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                    ),
+                     const Positioned(
+                    bottom: 115,
+                    left: 0,
+                    right: 0,
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      
+                      children: [
+                        Icon(
+                          Icons.bolt,
+                          color: Colors.yellow,
+                          size: 30,
+                        ),
+                        SizedBox(width: 0),
+                        Text(
+                          'Now Discussing',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                         Spacer(),
+                        CircleAvatar(
+                          //backgroundImage: AssetImage('assets/image/Flutter.png'),
+                          backgroundImage: AssetImage('assets/images/C#.png'),
+                          radius: 15,
+                        ),
+                         
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/Python.png'),
+                          radius: 15,
+                        ),
+                         
+                        CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/logo-UTEA.png'),
+                          radius: 15,
+                        ),
+                         SizedBox(width: 0),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          radius: 15,
+                          child: Text('+15', style: TextStyle(color: Colors.white, fontSize: 12)),
+                        ),
+                        SizedBox(width: 0),
+                      ],
+                    ),
+                    ),
+                    
+                    ),
+                    Positioned(
+                      bottom: 30,
+                      left: 0,
+                      right: 0,
+                      child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: ElevatedButton.icon(
+                        
+                        onPressed: (){},
+                        
+                        label: const Text('Comentar',style: TextStyle(color: Colors.white),),
+                        icon: const Icon(Icons.comment, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              padding:const EdgeInsets.symmetric(vertical: 18,horizontal: 15),
+                              minimumSize:const Size(double.infinity, 60), 
+                              textStyle: const TextStyle(fontSize: 20),
+                              
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(25),
+                              ),
+                            ),
+                      )
+                    ),
+                    
+
+                    ),
+
+        ],
+      ),
+      
+      
+    );
   }
 
 }
--- lib/vistas/paginas/preguntas/preguntas.dart
+++ lib/vistas/paginas/preguntas/preguntas.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Preguntas extends StatefulWidget {
@@ -10,32 +9,75 @@ class Preguntas extends StatefulWidget {
 }
 
 class _PreguntasState extends State<Preguntas> {
+  final List<Widget> score = [];
   final List<String> preguntas = [
     '¿El hombre llegó a la luna?',
     '¿La tierra es plana?',
     '¿El agua hierve a 100 grados Celsius?',
     '¿El sol es una estrella?',
-    'Tu flaca es plana?',
+    '¿Tu flaca es plana?',
   ];
 
   final List<bool> respuestas = [true, false, true, true, true];
   int actualPreguntaIndex = 0;
   String resultadoMensaje = '';
+  int aciertos = 0;
+  int desaciertos = 0;
+  bool juegoTerminado = false;
+  bool preguntaRespondida = false;
+  final Set<int> preguntasRespondidas = {};
 
   void _preguntaAleatoria() {
-    setState(() {
-      actualPreguntaIndex = Random().nextInt(preguntas.length);
-      resultadoMensaje = '';
-    });
+    if (preguntasRespondidas.length < preguntas.length) {
+      setState(() {
+        do {
+          actualPreguntaIndex = Random().nextInt(preguntas.length);
+        } while (preguntasRespondidas.contains(actualPreguntaIndex));
+        preguntasRespondidas.add(actualPreguntaIndex);
+        resultadoMensaje = '';
+        preguntaRespondida = false;
+      });
+    } else {
+      setState(() {
+        juegoTerminado = true;
+        resultadoMensaje =
+            'Juego terminado! Aciertos: $aciertos, Desaciertos: $desaciertos';
+      });
+    }
   }
 
   void _verificarRespuesta(bool respuestaUsuario) {
+    if (!juegoTerminado && !preguntaRespondida) {
+      setState(() {
+        preguntaRespondida = true;
+        if (respuestaUsuario == respuestas[actualPreguntaIndex]) {
+          resultadoMensaje = '¡Firme!';
+          score.add(Icon(Icons.check_circle_outline, color: Colors.green));
+          aciertos++;
+        } else {
+          resultadoMensaje = '¡Incorrecto!';
+          score.add(Icon(Icons.close, color: Colors.red));
+          desaciertos++;
+        }
+        if (preguntasRespondidas.length == preguntas.length-1) {
+          juegoTerminado = true;
+          resultadoMensaje =
+              'Juego terminado! Aciertos: $aciertos, Desaciertos: $desaciertos';
+        }
+      });
+    }
+  }
+
+  void _reiniciarJuego() {
     setState(() {
-      if (respuestaUsuario == respuestas[actualPreguntaIndex]) {
-        resultadoMensaje = '¡Firme!';
-      } else {
-        resultadoMensaje = '¡Incorrecto!';
-      }
+      score.clear();
+      aciertos = 0;
+      desaciertos = 0;
+      actualPreguntaIndex = 0;
+      resultadoMensaje = '';
+      juegoTerminado = false;
+      preguntasRespondidas.clear();
+      preguntaRespondida = false;
     });
   }
 
@@ -51,9 +93,7 @@ class _PreguntasState extends State<Preguntas> {
             fontFamily: 'Poppins',
             color: Colors.white,
             fontWeight: FontWeight.w600,
-            
           ),
-          
         ),
         backgroundColor: Colors.black,
       ),
@@ -65,7 +105,8 @@ class _PreguntasState extends State<Preguntas> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
-              preguntas[actualPreguntaIndex],
+              juegoTerminado ? resultadoMensaje : preguntas[actualPreguntaIndex],
+              
               textAlign: TextAlign.center,
               style: const TextStyle(
                 fontSize: 25,
@@ -73,80 +114,86 @@ class _PreguntasState extends State<Preguntas> {
                 fontFamily: 'Poppins',
               ),
             ),
-            Text(
-              resultadoMensaje,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 22,
-                color: Colors.white,
-                fontFamily: 'Poppins',
+            if (!juegoTerminado)
+              Text(
+                resultadoMensaje,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                ),
               ),
-            ),
             const SizedBox(height: 180),
             ElevatedButton(
-              autofocus:true,
-              onPressed: _preguntaAleatoria,
+              autofocus: true,
+              onPressed: juegoTerminado ? _reiniciarJuego : _preguntaAleatoria,
               style: ElevatedButton.styleFrom(
-                fixedSize:const Size(60, 60),
-                backgroundColor:const Color(0xFF383A6A),
+                fixedSize: const Size(60, 60),
+                backgroundColor: const Color(0xFF383A6A),
                 foregroundColor: Colors.white,
-                minimumSize:const Size(double.infinity, 50),
+                minimumSize: const Size(double.infinity, 50),
                 textStyle: const TextStyle(
                   fontFamily: 'Poppins',
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
-                  
                 ),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15),
                 ),
-                
               ),
-              child: const Text('Siguiente Pregunta'),
+              child: Text(juegoTerminado ? 'Volver a Jugar' : 'Siguiente Pregunta'),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _verificarRespuesta(true),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:const Color(0xFF01E1B7),
-                foregroundColor: Colors.white,
-                minimumSize:const Size(double.infinity, 100) ,
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Poppins',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600
+            if (!juegoTerminado) ...[
+              ElevatedButton(
+                onPressed: preguntaRespondida ? null : () => _verificarRespuesta(true),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF01E1B7),
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 100),
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
                 ),
-                shape:const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero,
-                ),
+                child: const Text('Verdadero'),
               ),
-              child: const Text('Verdadero'),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: preguntaRespondida ? null : () => _verificarRespuesta(false),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF94074),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  minimumSize: const Size(double.infinity, 100),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero, 
+                  ),
+                ),
+                child: const Text('Falso'),
+              ),
+            ],
+            const SizedBox(height: 20),
+            Row(
+              children: score,
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _verificarRespuesta(false),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:const Color(0xFFF94074),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Poppins',
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600
-                ),
-                minimumSize:const Size(double.infinity, 100),
-                shape:const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.zero, // Hace el botón cuadrado
-                ),
-              ),
-              child: const Text('Falso'),
-            ),
-            // const SizedBox(height: 20),
-            
           ],
         ),
       ),
     );
   }
 }
+
+
