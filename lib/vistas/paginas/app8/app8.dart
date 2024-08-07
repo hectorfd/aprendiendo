b04ba476ee7b929d921c25bd7752d18c231d624d
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-08-07 07:10:58 -0500

    progreso

--- lib/vistas/paginas/app8/app8.dart
+++ lib/vistas/paginas/app8/app8.dart
@@ -10,9 +10,37 @@ class App8 extends StatelessWidget{
       final double scaleFactor = screenSize.width > 600 ? 1.5 : 1.0;
       final double iconSize = screenSize.width > 600 ? 32.0 : 24.0;
     return Scaffold(
+      backgroundColor:const Color(0xFFF3F8FE),
       appBar: AppBar(
         title:Text('App 8 Hira Riaz', style: GoogleFonts.poppins(color: Colors.grey, fontSize: scaleFactor * 25, fontWeight: FontWeight.w600),),
 
+      ),
+      body: SafeArea(child:Padding(
+        padding: EdgeInsets.all(8.0 * scaleFactor),
+        child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(16 * scaleFactor),
+                  boxShadow: [BoxShadow(color: Colors.black45, offset: Offset(0, 7), blurRadius: 16 * scaleFactor)],
+        
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.align_horizontal_left,),
+                        Icon(Icons.more_vert,)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+        ),
+      ),
       ),
     );
   }
