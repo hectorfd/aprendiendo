a0eaa533be457bbecccf4d2a6e5c7b834024f47b
61565698+hectorfd@users.noreply.github.com <61565698+hectorfd@users.noreply.github.com>
2024-10-10 07:13:25 -0500

    aplicando youtube player ok

--- lib/vistas/paginas/youtubeprof/pages/video_detail_page.dart
+++ lib/vistas/paginas/youtubeprof/pages/video_detail_page.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:aprendiendo/vistas/paginas/youtubeprof/models/video_model.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:http/http.dart' as http; 
 import 'dart:convert'; 
-class VideoDetailPage extends StatelessWidget {
+class VideoDetailPage extends StatefulWidget {
   final VideoModel videoModel;
 
   const VideoDetailPage({Key? key, required this.videoModel}) : super(key: key);
+
+  @override
+  _VideoDetailPageState createState() => _VideoDetailPageState();
+}
+
+class _VideoDetailPageState extends State<VideoDetailPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoModel.id.videoId!,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +41,17 @@ class VideoDetailPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network(videoModel.snippet.thumbnails.high.url),
+            // Reproductor de YouTube
+            YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.amber,
+            ),
             const SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Text(
-                videoModel.snippet.title, 
+                widget.videoModel.snippet.title,
                 style: const TextStyle(fontSize: 16, color: Colors.white),
                 textAlign: TextAlign.justify,
               ),
@@ -31,43 +63,43 @@ class VideoDetailPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    "Duración: ${videoModel.duration}",
+                    "Duración: ${widget.videoModel.duration}",
                     style: const TextStyle(fontSize: 14, color: Colors.white54),
                   ),
                   Text(
-                    "Vistas: ${videoModel.viewCount}",
+                    "Vistas: ${widget.videoModel.viewCount}",
                     style: const TextStyle(fontSize: 14, color: Colors.white54),
                   ),
                 ],
               ),
             ),
             const SizedBox(height: 20),
-
-          
             Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Principales"),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[200], 
-                      ),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Principales"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[200],
                     ),
-                    const SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: const Text("Más recientes", style: TextStyle(color: Colors.white),),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[800], 
-                      ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Más recientes",
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ],
-                ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[800],
+                    ),
+                  ),
+                ],
               ),
-
+            ),
             const SizedBox(height: 10),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 16.0),
@@ -85,14 +117,26 @@ class VideoDetailPage extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             FutureBuilder<List<String>>(
-              future: videoModel.id.videoId != null ? fetchComments(videoModel.id.videoId!) : Future.value([]),
+              future: widget.videoModel.id.videoId != null
+                  ? fetchComments(widget.videoModel.id.videoId!)
+                  : Future.value([]),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(child: CircularProgressIndicator());
                 } else if (snapshot.hasError) {
-                  return const Center(child: Text("Error al cargar comentarios", style: TextStyle(color: Colors.white)));
+                  return const Center(
+                    child: Text(
+                      "Error al cargar comentarios",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text("No hay comentarios", style: TextStyle(color: Colors.white)));
+                  return const Center(
+                    child: Text(
+                      "No hay comentarios",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
                 }
 
                 final comments = snapshot.data!;
@@ -102,12 +146,13 @@ class VideoDetailPage extends StatelessWidget {
                   itemCount: comments.length,
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 10.0),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           CircleAvatar(
-                            child: Text('A'), 
+                            child: Text('A'),
                             backgroundColor: Colors.grey[800],
                             foregroundColor: Colors.white,
                           ),
@@ -118,24 +163,32 @@ class VideoDetailPage extends StatelessWidget {
                               children: [
                                 Text(
                                   '@usuario • hace 2 horas',
-                                  style: const TextStyle(color: Colors.white54),
+                                  style:
+                                      const TextStyle(color: Colors.white54),
                                 ),
                                 const SizedBox(height: 5),
                                 Text(
                                   comments[index],
-                                  style: const TextStyle(color: Colors.white),
+                                  style:
+                                      const TextStyle(color: Colors.white),
                                 ),
                                 const SizedBox(height: 5),
                                 Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.start,
                                   children: [
-                                    Icon(Icons.thumb_up, color: Colors.white70),
+                                    Icon(Icons.thumb_up,
+                                        color: Colors.white70),
                                     const SizedBox(width: 5),
-                                    Text('10', style: const TextStyle(color: Colors.white70)),
+                                    Text('10',
+                                        style: const TextStyle(
+                                            color: Colors.white70)),
                                     const SizedBox(width: 15),
-                                    Icon(Icons.thumb_down, color: Colors.white70),
+                                    Icon(Icons.thumb_down,
+                                        color: Colors.white70),
                                     const SizedBox(width: 30),
-                                    Icon(Icons.comment, color: Colors.white70),
+                                    Icon(Icons.comment,
+                                        color: Colors.white70),
                                   ],
                                 ),
                                 const SizedBox(height: 5),
